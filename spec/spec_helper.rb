4072a5be33dd96312642ce1b0460890bc072b917
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-17 23:28:53 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require_relative '../lib/operations.rb'
 
-  RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-  
-  config.order = 'default'
+RSpec.configure do |config|
+  config.add_formatter 'documentation'
+end
 end
