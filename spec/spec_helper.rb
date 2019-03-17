23d70d12d27c28740841406badbd9160b07161e6
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-17 23:32:39 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require_relative '../lib/operations.rb'
+require_relative '../spec/operations_spec.rb'
 
 RSpec.configure do |config|
 
