564057c7d6fb2ac5e4753c02c5214d810c3b4156
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-17 23:37:19 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,26 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require_relative '../lib/operations.rb'
 
-RSpec.configure do |config|
-  
+RSpec.configure do |c|
+  c.add_setting :custom_setting
+end
+
+RSpec.describe "custom setting" do
+  it "is nil by default" do
+    expect(RSpec.configuration.custom_setting).to be_nil
+  end
+
+  it "acts false by default" do
+    expect(RSpec.configuration.custom_setting).to be_falsey
+  end
+
+  it "is exposed as a predicate" do
+    expect(RSpec.configuration.custom_setting?).to be_falsey
+  end
+
+  it "can be overridden" do
+    RSpec.configuration.custom_setting = true
+    expect(RSpec.configuration.custom_setting).to be_truthy
+    expect(RSpec.configuration.custom_setting?).to be_truthy
+  end
 end
